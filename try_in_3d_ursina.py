d225a2569bc75300547b594c14093c49afac14ad
jonycat <eldarakaev39@gmail.com>
2022-02-08 15:59:21 +0300

    make a test pickup

--- try_in_3d_ursina.py
+++ try_in_3d_ursina.py
@@ -1,7 +1,9 @@
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina import *
 app = Ursina()
-#
+
+
+
 class Ground(Entity):
     def __init__(self):
         super().__init__(
@@ -11,15 +13,31 @@ class Ground(Entity):
             scale = 500,
             position = Vec3(0, 0, 0),
             collider = 'plane')
+class Enemy(Entity):
+    def __init__(self):
+        super().__init__(parent = scene,
+                         model = 'cube',
+                         texture = 'color',
+                         scale = 5,
+                         position = (10,2.5,0),collider = "box")
+
+pickup = Entity(model="sphere", position=(1,.5,3))
 
 
-player = FirstPersonController(model='cube',
-                            texture = "None",
-                            scale=0.75,
-                            collider = 'box',
-                              speed = 2.255)
-camera.z = -5
+player = FirstPersonController(has_pickup = False)
 ground = Ground()
+Sky()
+Enemy()
+
+
+
+def update():
+    if not player.has_pickup and distance(player, pickup) < pickup.scale_x :
+        print("pickup")
+
+        player.has_pickup = True
+        pickup.animate_scale(0,duration=.1)
+        destroy(pickup,delay=.1)
 
 
 
