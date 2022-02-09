496f80d4effa7b0615859d5b0a2ea1a551ecad73
jonycat <eldarakaev39@gmail.com>
2022-02-09 11:11:53 +0300

    if pickup = play screamer (test screamer def)

--- try_in_3d_ursina.py
+++ try_in_3d_ursina.py
@@ -1,5 +1,7 @@
 from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina import *
+
+
 app = Ursina()
 
 
@@ -13,6 +15,7 @@ class Ground(Entity):
             scale = 500,
             position = Vec3(0, 0, 0),
             collider = 'plane')
+
 class Enemy(Entity):
     def __init__(self):
         super().__init__(parent = scene,
@@ -22,9 +25,23 @@ class Enemy(Entity):
                          position = (10,2.5,0),collider = "box")
 
 pickup = Entity(model="sphere", position=(1,.5,3))
+
+def Ambient():
+    pass
+
+
 def Screamer():
     video = 'video.mp4'
     video_player = Entity(model='quad', parent=camera.ui, scale= (2,1), texture=video)
+    a = audio.Audio(sound_file_name='sound.mp3', loop=True)
+    loaded_sound = app.loader.loadSfx('sound.mp3')
+    a.clip = loaded_sound
+
+
+
+
+
+
 
 
 player = FirstPersonController(has_pickup = False)
@@ -35,6 +52,7 @@ Enemy()
 
 
 def update():
+
     if not player.has_pickup and distance(player, pickup) < pickup.scale_x :
 
 
